6d7dd43df6c5c2a067616f807d69b06fb4eb6b84
KemoKemo <t2wonderland@gmail.com>
2016-09-17 17:06:35 +0900

    vbsを実行するためのバッチファイルを追加。

---
+++ scripts/CollectAllEventLogs.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem ■概要説明
+rem テキストでイベントログを回収するサンプル。
+rem Application、System、Securityの主要な3つのログを回収する。
+
+rem ■設定事項
+set original=
+set target=
+
+rem ■このバッチが存在するフォルダをカレントにして、標準出力をクリア
+pushd %0\..
+cls
+
+rem ■本処理
+call :PreCausion
+call :CollectEventLogs
+goto :End
+
+rem 管理者実行しないと回収できないログがあるので、前もって警告
+:PreCausion
+echo.
+echo ****
+echo You need execute as an Administrator to collect the Security event logs.
+echo ****
+echo.
+exit /b
+
+rem 主要3つのイベントログをcsvファイルで回収
+:CollectEventLogs
+cscript DumpEventLog.vbs 127.0.0.1 AllEventLogs.csv /all
+exit /b
+
+:End
+
+endlocal
+pause
+exit
